cd3ab5c0a90e94f067400d210464050832ca9efd
Iván Pérez <pablopazojim@gmail.com>
2020-09-07 18:12:49 +0200

    Cambios pregunta 3

--- docker/docker-settings2.py
+++ docker/docker-settings2.py
@@ -14,7 +14,7 @@ STATIC_ROOT = '/app/static/'
 MEDIA_ROOT = '/app/static/media/'
 ALLOWED_HOSTS = ['*']
 
-# Modules in use, commented modules that you won't use
+# Modules in use, commented modulesthat you won't use
 MODULES = [
     'authentication',
     'base',
